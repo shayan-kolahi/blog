34163c11d6eea7750095b12d15840e5f47ef895b
shayan kolahi <shayankolahi30@gmail.com>
2025-02-20 15:39:09 +0330

    make component sidebar and main for home page and change style scroll body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { PrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import {HeaderComponent} from './components/header/header.component';
+import {MainComponent} from './components/main/main.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
 
 
 @Component({
   selector: 'app-root',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    SidebarComponent
   ],
   templateUrl: './app.component.html'
 })
