e69791e575a0db3bcce50ad04ec226ba399e0589
shayan kolahi <shayankolahi30@gmail.com>
2025-03-01 16:42:02 +0330

    make page new-post

--- src/app/pages/new-post/new-post.component.ts
+++ src/app/pages/new-post/new-post.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HeaderComponent} from '../../components/header/header.component';
+import {FormsModule} from '@angular/forms';
+import {MessageModule} from 'primeng/message';
+import {MultiSelectModule} from 'primeng/multiselect';
 
 @Component({
   selector: 'app-new-post',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    FormsModule,
+    MultiSelectModule,
+    MessageModule
   ],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.scss'
 })
 export class NewPostComponent {
-
+  title: any;
+  description: any;
+  text: any;
+  cities: any[] | undefined = [
+    {name: 'New York', code: 'NY'},
+    {name: 'Rome', code: 'RM'},
+    {name: 'London', code: 'LDN'},
+    {name: 'Istanbul', code: 'IST'},
+    {name: 'Paris', code: 'PRS'}
+  ];
+  selectedCities: any;
 }
